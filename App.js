a04a73d379b43405414e8e9331d5ddf57c5a38f4
hangpy <leehangbok2009@gmail.com>
2018-12-16 23:53:55 +0900

    organazing top navigator basic structure

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import BottomNavigator from "./components/BottomNavigator";
+import TopNavigator from "./components/TopNavigator";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,6 +23,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <TopNavigator/>
         <BottomNavigator/>
       </View>
     );
@@ -30,7 +32,6 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-
+  flex: 1,
   }
 });
--- components/TopNavigator.js
+++ components/TopNavigator.js
@@ -6,14 +6,24 @@
 
 import React, { Component } from 'react';
 import { Platform, View, StyleSheet, Text, Image, AppRegistry } from 'react-native';
+import { Header } from "react-native-elements";
 
 export default class TopNavigator extends Component <{}> {
 
 	render() {
 		return (
-		    <View style={styles.container}>
-		        <Text>Top Navigator</Text>
-		    </View>
+			<Header
+				leftComponent={{ icon: 'search', color: '#000' }}
+				centerComponent={{ text: 'COODIE', style: { color: '#000' } }}
+				rightComponent={{ icon: 'navigation', color: '#000' }}
+				backgroundColor={'#fff'}
+				containerStyle={{
+					shadowColor: '#000',
+                    shadowOffset: { width: 1, height: 1 },
+                    shadowOpacity: 0.1,
+                    shadowRadius: 3,
+				}}
+			/>
         );
 	}
 }
